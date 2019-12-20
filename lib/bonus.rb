39a68dd8a40ae71d4df16483e3a1d13ac60ffeec
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-20 15:38:38 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,8 +19,11 @@ def bonus
       ]
    }
   }
+  
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-  #code your solution here:
+  
 
   
 
